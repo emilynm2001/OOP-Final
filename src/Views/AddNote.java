573c57c25d609d2a996a0070381e6200bf8a40e0
hernandezj0174 <hernandezj0174@gmail.com>
2022-12-03 18:59:34 -0600

    Added Alert Box
    Alert box appears when user hits the save button and the title of the note already exists.

--- src/Views/AddNote.java
+++ src/Views/AddNote.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 
 import Frames.NotedFrame;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 // import javafx.scene.layout.HBox;
 // import Frames.*;
@@ -84,6 +87,9 @@ public class AddNote extends AnchorPane {
         saveButton.setOnMouseClicked(e -> {
             if (!newNote.fileExists) {
                 NotedFrame.getMainPane().getState(0);
+            } else {
+                Alert alert = new Alert(AlertType.CONFIRMATION, "Cannot create new note with already existing title. Please select a new note name.", ButtonType.OK);
+                alert.showAndWait();
             }
         });
 
